0c6e726cbe42cddd958409131515a189d2261785
Jobert Simbre <jobert.simbre14@gmail.com>
2023-10-07 04:47:23 -0700

    crime list

--- lib/crime/fetch_data.php
+++ lib/crime/fetch_data.php
@@ -27,7 +27,7 @@ $columns = array(
         'db'        => 'coordinates',
         'dt'        => 6,
         'formatter' => function ($d, $row) {
-            $d = '<a href="https://www.google.com/maps/place/' . $d . '" target="_blank"><i class="bi bi-map"></i> View Location</a>';
+            $d = '<a class="btn btn-primary" href="https://www.google.com/maps/place/' . $d . '" target="_blank"><i class="bi bi-map"></i> View Location</a>';
             return $d;
         }
     )
